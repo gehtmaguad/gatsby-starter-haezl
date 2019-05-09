e047ccd65b2a0e654ba63551627c9c48608a696b
Markus Hösel <mail@hoeselm.at>
2019-05-09 08:16:30 +0200

    update gatsby-browser.js: inform on new updates in pwa

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,15 @@
 import "./src/styles/global.css"
 require("prismjs/themes/prism-solarizedlight.css")
+
+export const onServiceWorkerUpdateFound = () => {
+  if (isBrowser) {
+    const answer = window.confirm(
+      `There was a change in the content of this webapp. ` +
+        `Pull down in order to fetch the latest update!`
+    )
+
+    if (answer === true) {
+      window.location.reload()
+    }
+  }
+}
