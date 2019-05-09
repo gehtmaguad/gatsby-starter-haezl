b0590d45405f2ac77df23f19233b5944cf3f9179
Markus Hösel <mail@hoeselm.at>
2019-05-09 08:44:10 +0200

    fix onServiceWorkerUpdateFound

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,14 +2,12 @@ import "./src/styles/global.css"
 require("prismjs/themes/prism-solarizedlight.css")
 
 export const onServiceWorkerUpdateFound = () => {
-  if (isBrowser) {
-    const answer = window.confirm(
+  if (
+    window.confirm(
       `There was a change in the content of this webapp. ` +
         `Pull down in order to fetch the latest update!`
     )
-
-    if (answer === true) {
-      window.location.reload()
-    }
+  ) {
+    window.location.reload(true)
   }
 }
