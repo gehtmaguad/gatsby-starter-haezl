16cced9bcd08022806ea28d546571ef87447fe79
Markus Hösel <mail@hoeselm.at>
2019-05-10 09:12:33 +0200

    refactoring onServiceWorkerUpdateFound hook

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,11 +2,6 @@ import "./src/styles/global.css"
 require("prismjs/themes/prism-solarizedlight.css")
 
 export const onServiceWorkerUpdateFound = () => {
-  if (
-    window.confirm(
-      `There was a change in the content of this webapp. \nReload webapp?`
-    )
-  ) {
-    window.location.reload(true)
-  }
+  console.log("onServiceWorkerUpdateFound: reloading page")
+  window.location.reload(true)
 }
