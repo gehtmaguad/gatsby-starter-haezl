6a7aa498d041526eda833b7dfeed16b40e4881d9
Markus Hösel <mail@hoeselm.at>
2019-05-12 20:59:42 +0200

    outcommented log statement in onServiceWorkerUpdateReady hook

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
 import "./src/styles/global.css"
 require("prismjs/themes/prism-solarizedlight.css")
 
-export const onServiceWorkerUpdateReady = () => {
-  console.log(
-    "onServiceWorkerUpdateReady hook triggered: force reloading page."
-  )
-  // window.location.reload(true)
-}
+// export const onServiceWorkerUpdateReady = () => {
+// console.log(
+//   "onServiceWorkerUpdateReady hook triggered: force reloading page."
+// )
+// window.location.reload(true)
+// }
